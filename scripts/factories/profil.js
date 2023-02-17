ed4fb97b6bf6c9cdd0a920319a2b15df71ae428b
Adman59 <adrien.magnies@hotmail.fr>
2023-02-17 15:45:20 +0100

    Import des données selon l'ID du photographe + style

--- scripts/factories/profil.js
+++ scripts/factories/profil.js
@@ -3,16 +3,28 @@ function photographerDetailFactory(data) {
   const { name, portrait, city, country, tagline, price, id } = data;
   const picture = `../assets/photographers/${portrait}`;
 
+
   function displayHeaderPhotographer() {
+
     let headerphotographer =
       `
-      <div class="photographer-desc">
-        <h3>${city}, ${country}</h3>
-        <p class="photogapher-tagline">${tagline}</p>
-      </div>
+        <div class="profil-photographer">
+          <h1>${name}</h1>
+          <p class="photogapher-location">${city}, ${country}</p>
+          <p class="photogapher-tagline">${tagline}</p>
+        </div>
+
+        <div class="photographer-contact">
+            <button type="button" class="contact_button" id="contact-btn" aria-label="Contact Me">Contactez-moi</button>
+        </div>
+
+        <div class="photographer-img">
+          <img src="${picture}" alt="${name}">
+        </div>  
         `;
     return headerphotographer;
   }
 
   return { name, portrait, city, country, tagline, price, id, picture, displayHeaderPhotographer };
 }
+
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,43 +2,59 @@
 
 // La fonction getPhotographers permet de récupérer les données des photographes (le async await permet d'attendre que les données soient récupérées pour s'afficher) :
 
+let params = (new URL(document.location)).searchParams;
+let photographer_id = params.get('id');
+let photographer;
 
-async function getPhotographers() {
+console.log(params);
+console.log(photographer_id);
 
-    await fetch("../data/photographers.json")
+//--------------------------------------------------------//
+
+async function getPhotographer() {
+    // Recuperation du fichier JSON en utilisant "fetch".
+    return fetch('../data/photographers.json')
         .then((res) => res.json())
-        .then((data) => (photographers = data.photographers));
-    // et bien retourner le tableau photographers seulement une fois récupéré
-    return {
-        photographers: [...photographers]
-    };
+        .then((data) => (photographers = data.photographers))
+        .then(function (photographers) {
+            let result = null;
+            photographers.forEach(element => {
+                if (element.id == photographer_id) {
+                    result = element;
+                }
+            });
+
+            return result;
+        })
+        .catch(function (err) {
+            // Une erreur est survenue
+            console.log(err)
+        });
 }
 
-
+//--------------------------------------------------------//
 
 // La fonction displayData permet d'afficher...
-
 // affichage de la section photographer_header
 async function displayData(photographer) {
     // Photographer's detail
-    photographers.forEach((photographer) => {
-        const photographersHeader = document.querySelector(".photograph-header");
-        const photographerDetails = photographerDetailFactory(photographer);
-        const userCardDOM = photographerDetails.displayHeaderPhotographer();
+    const photographersHeader = document.querySelector(".photograph-header");
+    const photographerDetails = photographerDetailFactory(photographer);
+    const userCardDOM = photographerDetails.displayHeaderPhotographer();
 
-        photographersHeader.insertAdjacentHTML('beforeend', userCardDOM);
-    })
+    photographersHeader.insertAdjacentHTML('beforeend', userCardDOM);
 };
 
-
-
+//--------------------------------------------------------//
 
 async function init() {
     // Récupère les datas des photographes
-    photographer = await getPhotographers();
+    photographer = await getPhotographer();
 
     displayData(photographer);
 
 };
+
+//--------------------------------------------------------//
 
 init();
