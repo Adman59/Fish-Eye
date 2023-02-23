3928e1b5e5c5b15a95bd5054f569b516abba0717
Adman59 <adrien.magnies@hotmail.fr>
2023-02-23 17:15:21 +0100

    Ajout des médias dans la page photographes + CSS

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,28 +1,74 @@
-class mediaFactory {
+class MediaFactory {
     constructor(data) {
-        const { id, photographerId, title, image, likes, date, price } = data;
-        this.id = id;
-        this.photographerId = photographerId;
-        this.title = title;
-        this.image = image;
-        this.likes = likes;
-        this.date = date;
-        this.price = price;
+        if (data.image) {
+            return new Img(data);
+        } else if (data.video) {
+            return new Video(data);
+        } else {
+            throw "Format d'image inconnu";
+        }
     }
+}
+class Img {
+    constructor(data) {
+        this.image = data.image;
+        this.id = data.id;
+        this.photographerId = data.photographerId;
+        this.title = data.title;
+        this.likes = data.likes;
+        this.tags = data.tags;
+        this.date = data.date;
+        this.alt = data.alt;
+    }
+    createHtml() {
+        return `
+        <article class="media-photographer">
+          <div class="media">
+            <img src="./assets/images/${this.photographerId}/${this.image}" alt="${this.alt}" class="media-thumbnail" tabindex="5"/>
+          </div>
+         <div class="media-description">
+              <h3>${this.title}</h3>
+            <div class="like">
+                <span class="numbers">${this.likes}</span>
+                <span class="hearts" data-like="false" tabindex="5">
+                  <i class="fas fa-heart" alt="coeur" aria-label="ajoute ou supprime le like au clic">
+                  </i>
+                </span> 
+            </div>
+          </div>
+        </article>
+          `;
+    }
+}
 
-    get displayHeaderPhotographer() {
-
-        //Elements du DOM
-        let articlephotograper =
-            `
-            <article class="card-photographer">
-                <div>TEST TEST TEST TEST TEST</div>
-            </article>
-            `
-            ;
-
-        return (articlephotograper);
-
-
+class Video {
+    constructor(data) {
+        this.video = data.video;
+        this.id = data.id;
+        this.photographerId = data.photographerId;
+        this.title = data.title;
+        this.likes = data.likes;
+        this.tags = data.tags;
+        this.date = data.date;
+        this.alt = data.alt;
+    }
+    createHtml() {
+        return `
+        <article class="media-photographer">
+            <div class="media">
+                    <video src="./assets/images/${this.photographerId}/${this.video}" controls type="video/mp4" alt="${this.alt}" class="media-thumbnail" tabindex="5"> </video>
+            </div>
+            <div class="media-description">
+                <h3>${this.title}</h3>
+                <div class="like">
+                    <span class="numbers">${this.likes}</span>
+                    <span class="hearts" data-like="false" tabindex="5">
+                    <i class="fas fa-heart" alt="coeur" aria-label="ajoute ou supprime le like au clic">
+                    </i>
+                    </span>
+                </div>
+            </div>
+        </article>
+        `;
     }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -5,13 +5,14 @@
 let params = (new URL(document.location)).searchParams;
 let photographer_id = params.get('id');
 let photographer;
+let media;
 
 console.log(params);
 console.log(photographer_id);
 
 //--------------------------------------------------------//
 
-async function getPhotographer() {
+function getPhotographer() {
     // Recuperation du fichier JSON en utilisant "fetch".
     return fetch('../data/photographers.json')
         .then((res) => res.json())
@@ -34,6 +35,27 @@ async function getPhotographer() {
 
 //--------------------------------------------------------//
 
+function getMedias() {
+    const sectionMedia = document.querySelector('.medias-section');
+
+    // Recuperation du fichier JSON en utilisant "fetch".
+    return fetch('../data/photographers.json')
+        .then((res) => res.json())
+        .then((data) => {
+            const filteredMedias = data.media.filter((media) => media.photographerId == photographer_id);
+            console.log(filteredMedias);
+            return filteredMedias;
+        })
+        .catch(function (err) {
+            // Une erreur est survenue
+            console.log(err)
+        });
+}
+
+
+
+//--------------------------------------------------------//
+
 // La fonction displayData permet d'afficher...
 // affichage de la section photographer_header
 async function displayData(photographer) {
@@ -47,11 +69,30 @@ async function displayData(photographer) {
 
 //--------------------------------------------------------//
 
+async function displayMedia(mediaArray) {
+    const gallery = document.querySelector('.medias-section');
+
+    try {
+        mediaArray.forEach((media) => {
+            const factory = new MediaFactory(media);
+            const mediaHtml = factory.createHtml();
+            gallery.innerHTML += mediaHtml;
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+//--------------------------------------------------------//
+
 async function init() {
     // Récupère les datas des photographes
     photographer = await getPhotographer();
+    media = await getMedias();
 
     displayData(photographer);
+    displayMedia(media);
 
 };
 
