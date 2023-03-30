86a7d835d958eaa7a59384e249c561c7ce1668cb
Adman59 <adrien.magnies@hotmail.fr>
2023-03-30 10:05:45 +0200

    Ajout du HTML Lang FR

--- scripts/utils/mediasModal.js
+++ scripts/utils/mediasModal.js
@@ -23,7 +23,7 @@ function initMediasModal(medias) {
                 
                 <span class="next-button"></span>
             </div>
-            <button id="lightbox-close-btn" class="close-button" alt="Close dialog">
+            <button id="lightbox-close-btn" class="close-button" alt="Close dialog" aria-label="close dialog button">
                 <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M42 4.23L37.77 0L21 16.77L4.23 0L0 4.23L16.77 21L0 37.77L4.23 42L21 25.23L37.77 42L42 37.77L25.23 21L42 4.23Z"
